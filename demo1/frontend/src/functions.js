d75bc9cfbd0b03e4cd0922d9b00be7a3ff595fa4
brhaspati <3rhaspati@gmail.com>
2018-06-26 16:06:25 +0100

    updated bot function

--- demo1/frontend/src/functions.js
+++ demo1/frontend/src/functions.js
@@ -1,12 +1,32 @@
-var subscribeURL = "https://d67ryh9knh.execute-api.eu-west-1.amazonaws.com/dev"
+var URL = "https://anfc79ka6d.execute-api.eu-west-1.amazonaws.com/dev"
 
+
+function submitBot(){
+var honeyURL = URL + "/v1/get-pass"
+ var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", honeyURL, true); // true for asynchronous 
+    xmlHttp.send(null);
+    var email = "bot"+ Math.floor((Math.random() * 1000000) + 1) +"@botmail.com";
+    $.ajax(URL + "/subscribe", {
+    	method: 'POST',
+    	contentType: 'application/json',
+        data: JSON.stringify({email: email}),
+	beforeSend: function(xmlHttpRequest) {
+     		xmlHttpRequest.withCredentials = true;
+        }
+     })
+}
 
 function submit(){
 
 			
 			var email = ""+document.getElementById("email").value;
 			
-			$.ajax(subscribeURL + "/subscribe", {
+			$.ajax(URL + "/subscribe", {
     				method: 'POST',
     				contentType: 'application/json',
                     data: JSON.stringify({
